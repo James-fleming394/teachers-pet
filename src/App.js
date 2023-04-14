8e0efcca11405c8836f9641cb30c44ad340b4f2d
james-fleming394 <james.fleming394@gmail.com>
2023-04-13 21:01:45 -0400

    Test and quizzes pages

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Search from './components/Search';
 import { Route, Routes } from 'react-router';
 import Home from './pages/Home';
 import Assessments from './pages/Assessments';
+import Tests from './pages/Tests';
+import Quizzes from './pages/Quizzes';
 
 const App = () => {
   return (
@@ -15,7 +17,8 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/assessments" element={<Assessments />} />
-            
+              <Route path="/assessments/tests" element={<Tests />} />
+              <Route path="/assessments/quizzes" element={<Quizzes />} />
         </Routes>
       </div>
     </div>
