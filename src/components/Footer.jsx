47b7ec2d52cac8f587e2dd9b81c19e54a2c64a52
james-fleming394 <james.fleming394@gmail.com>
2023-04-19 12:29:09 -0400

    Footer header

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import './Footer.css';
 
 const Footer = () => {
 
     return (
         <div className="footer">
-            
+            <div className="footer-join">
+                <h1 className="footer-h1">Join our global community <br></br>of passionate educators today.</h1>
+                <button className="footer-btn">
+                    Sign Up
+                </button>
+            </div>
         </div>
     )
 }
