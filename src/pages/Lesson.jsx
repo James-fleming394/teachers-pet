d9fd43ba38c23b9d149a90997ba8cdd0d59a13dc
james-fleming394 <james.fleming394@gmail.com>
2023-04-19 16:28:38 -0400

    Lessons page layout

--- src/pages/Lesson.jsx
+++ src/pages/Lesson.jsx
@@ -77,6 +77,209 @@ const Lesson = () => {
                     alt="lessons" />
                 </div>
             </div>
+            <div className="featured">
+                <h1 className="assess-all">Featured Math Lessons</h1>
+                <div className="features-container">
+                    <div className="feature">
+                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/STEM-Bins-STEAM-Bins-STEM-Activities-2719619-1658944359/original-2719619-1.jpg"
+                        className="feature-image"
+                        alt="feature" />
+                        <h4 className="feature-title">STEM Bins Lesson</h4>
+                        <p className="author">Ms. Besio</p>
+                        <p className="star"><img className="star-" src={RatingLogo} alt="rating"/>4.9</p>
+                    </div>
+                    <div className="feature">
+                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/Counting-Money-Game--549035-1681834203/original-549035-1.jpg"
+                        className="feature-image"
+                        alt="feature" />
+                        <h4 className="feature-title">Money Math Game Lesson</h4>
+                        <p className="author">Mr. Swingle</p>
+                        <p className="star"><img src={RatingLogo} alt="rating"/>4.8</p>
+                    </div>
+                    <div className="feature">
+                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/Fraction-Activities-Fraction-Unit-Printable-Worksheets-Lessons-Assessments-1016354-1680543197/original-1016354-1.jpg"
+                        className="feature-image"
+                        alt="feature" />
+                        <h4 className="feature-title">Fractions Lessons: Grade 4</h4>
+                        <p className="author">Ms. Fagan</p>
+                        <p className="star"><img src={RatingLogo} alt="rating"/>4.9</p>
+                    </div>
+                </div>
+            </div>
+            <div className="featured">
+                <h1 className="assess-all">Featured English Lessons</h1>
+                <div className="features-container">
+                    <div className="feature">
+                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/Interactive-Reading-Notebooks-Informational-Text-Nonfiction-for-Common-Core-4-8-1668076774/original-790642-1.jpg"
+                        className="feature-image"
+                        alt="feature" />
+                        <h4 className="feature-title">Interactive Reading: Non-Fiction Texts</h4>
+                        <p className="author">Ms. Manca</p>
+                        <p className="star"><img className="star-" src={RatingLogo} alt="rating"/>4.6</p>
+                    </div>
+                    <div className="feature">
+                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/Phonics-Poetry-for-Grades-K-2-1475734-1674900096/original-1475734-1.jpg"
+                        className="feature-image"
+                        alt="feature" />
+                        <h4 className="feature-title">Phonics Poetry Lesson</h4>
+                        <p className="author">Ms. Brennan</p>
+                        <p className="star"><img src={RatingLogo} alt="rating"/>4.9</p>
+                    </div>
+                    <div className="feature">
+                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/Interactive-Reading-Journal-Notebook-for-K-2-Common-Core-Aligned-073763000-1378165324-1681133125/original-858335-1.jpg"
+                        className="feature-image"
+                        alt="feature" />
+                        <h4 className="feature-title">Interactive Reading Notebook</h4>
+                        <p className="author">Ms. Hall</p>
+                        <p className="star"><img src={RatingLogo} alt="rating"/>4.4</p>
+                    </div>
+                </div>
+            </div>
+            <div className="featured">
+                <h1 className="assess-all">Featured Writing Lessons</h1>
+                <div className="features-container">
+                    <div className="feature">
+                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/Common-Core-Writing-for-1st-Grade-Narratives-Informative-Opinion-Bundle-1657552962/original-487663-1.jpg"
+                        className="feature-image"
+                        alt="feature" />
+                        <h4 className="feature-title">First Grade Writing Lessons</h4>
+                        <p className="author">Ms. Pealone</p>
+                        <p className="star"><img className="star-" src={RatingLogo} alt="rating"/>4.7</p>
+                    </div>
+                    <div className="feature">
+                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/Writing-Center-Printables-Interactive-Learning-Display-853834-1678980824/original-853834-1.jpg"
+                        className="feature-image"
+                        alt="feature" />
+                        <h4 className="feature-title">Interactive Writing Lessons/Centers</h4>
+                        <p className="author">Ms. Bryant</p>
+                        <p className="star"><img src={RatingLogo} alt="rating"/>4.8</p>
+                    </div>
+                    <div className="feature">
+                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/The-Day-the-Crayons-Quit-Opinion-Writing-2495386-1657522048/original-2495386-1.jpg"
+                        className="feature-image"
+                        alt="feature" />
+                        <h4 className="feature-title">Opinionated Writing Lessons: 10 Day Plan</h4>
+                        <p className="author">Ms. Reagan</p>
+                        <p className="star"><img src={RatingLogo} alt="rating"/>4.9</p>
+                    </div>
+                </div>
+            </div>
+            <div className="featured">
+                <h1 className="assess-all">Featured Science Lessons</h1>
+                <div className="features-container">
+                    <div className="feature">
+                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/KinderScience-Kindergarten-Science-Curriculum-Homeschool-Compatible--2739645-1676993479/original-2739645-1.jpg"
+                        className="feature-image"
+                        alt="feature" />
+                        <h4 className="feature-title">Kindergarten Science Curriculum</h4>
+                        <p className="author">Mr. Fleming</p>
+                        <p className="star"><img className="star-" src={RatingLogo} alt="rating"/>4.9</p>
+                    </div>
+                    <div className="feature">
+                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/Apple-Science-Interactive-Activities-1444752-1681123182/original-1444752-1.jpg"
+                        className="feature-image"
+                        alt="feature" />
+                        <h4 className="feature-title">Apple Science Lessons</h4>
+                        <p className="author">Ms. Reginoe</p>
+                        <p className="star"><img src={RatingLogo} alt="rating"/>4.7</p>
+                    </div>
+                    <div className="feature">
+                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/Weather-and-Climate-Interactive-Notebook-BUNDLE-by-Science-Doodles-1550560-1657525533/original-1550560-1.jpg"
+                        className="feature-image"
+                        alt="feature" />
+                        <h4 className="feature-title">Weather & Climate Interactive Notebook Lessons</h4>
+                        <p className="author">Ms. Wallace</p>
+                        <p className="star"><img src={RatingLogo} alt="rating"/>4.8</p>
+                    </div>
+                </div>
+            </div>
+            <div className="featured">
+                <h1 className="assess-all">Featured Social Studies Lessons</h1>
+                <div className="features-container">
+                    <div className="feature">
+                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/Little-SOCIAL-STUDIES-Thinkers-UNIT-1-Citizenship-Kindergarten-Social-Studies--2689773-1664967206/original-2689773-1.jpg"
+                        className="feature-image"
+                        alt="feature" />
+                        <h4 className="feature-title">Social Studies: Citizenship Lessons</h4>
+                        <p className="author">Ms. Scott</p>
+                        <p className="star"><img className="star-" src={RatingLogo} alt="rating"/>4.6</p>
+                    </div>
+                    <div className="feature">
+                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/Colony-Unit-filled-with-Informational-Text-Quiz-Test-Project-1668958073/original-510660-1.jpg"
+                        className="feature-image"
+                        alt="feature" />
+                        <h4 className="feature-title">Social Studies: Colonial Unit Lessons</h4>
+                        <p className="author">Ms. Weinberg</p>
+                        <p className="star"><img src={RatingLogo} alt="rating"/>4.9</p>
+                    </div>
+                    <div className="feature">
+                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/Holidays-and-Christmas-Around-the-World-Digital-and-Printable-2885336-1671833250/original-2885336-1.jpg"
+                        className="feature-image"
+                        alt="feature" />
+                        <h4 className="feature-title">Holidays Around the World Lesson</h4>
+                        <p className="author">Ms. Cayne</p>
+                        <p className="star"><img src={RatingLogo} alt="rating"/>4.7</p>
+                    </div>
+                </div>
+            </div>
+            <div className="featured">
+                <h1 className="assess-all">Featured Social Studies Lessons</h1>
+                <div className="features-container">
+                    <div className="feature">
+                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/Little-SOCIAL-STUDIES-Thinkers-UNIT-1-Citizenship-Kindergarten-Social-Studies--2689773-1664967206/original-2689773-1.jpg"
+                        className="feature-image"
+                        alt="feature" />
+                        <h4 className="feature-title">Social Studies: Citizenship Lessons</h4>
+                        <p className="author">Ms. Scott</p>
+                        <p className="star"><img className="star-" src={RatingLogo} alt="rating"/>4.6</p>
+                    </div>
+                    <div className="feature">
+                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/Colony-Unit-filled-with-Informational-Text-Quiz-Test-Project-1668958073/original-510660-1.jpg"
+                        className="feature-image"
+                        alt="feature" />
+                        <h4 className="feature-title">Social Studies: Colonial Unit Lessons</h4>
+                        <p className="author">Ms. Weinberg</p>
+                        <p className="star"><img src={RatingLogo} alt="rating"/>4.9</p>
+                    </div>
+                    <div className="feature">
+                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/Holidays-and-Christmas-Around-the-World-Digital-and-Printable-2885336-1671833250/original-2885336-1.jpg"
+                        className="feature-image"
+                        alt="feature" />
+                        <h4 className="feature-title">Holidays Around the World Lesson</h4>
+                        <p className="author">Ms. Cayne</p>
+                        <p className="star"><img src={RatingLogo} alt="rating"/>4.7</p>
+                    </div>
+                </div>
+            </div>
+            <div className="featured">
+                <h1 className="assess-all">Featured Other Lessons</h1>
+                <div className="features-container">
+                    <div className="feature">
+                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/First-Grade-Back-to-School-Activities-First-Week-Beginning-of-the-Year-BUNDLE-139226-1664282753/original-139226-1.jpg"
+                        className="feature-image"
+                        alt="feature" />
+                        <h4 className="feature-title">Back to School Activites 1st Grade</h4>
+                        <p className="author">Ms. Munson</p>
+                        <p className="star"><img className="star-" src={RatingLogo} alt="rating"/>4.9</p>
+                    </div>
+                    <div className="feature">
+                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/Back-to-School-Math-Activities-for-the-First-Week-of-School-297895-1661609294/original-297895-1.jpg"
+                        className="feature-image"
+                        alt="feature" />
+                        <h4 className="feature-title">Back to School Activites Kindergarten</h4>
+                        <p className="author">Ms. Spina</p>
+                        <p className="star"><img src={RatingLogo} alt="rating"/>4.9</p>
+                    </div>
+                    <div className="feature">
+                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/Summer-Theme-Preschool-Lesson-Plan-and-Summer-Activities-BUNDLE-2-3750691-1657528143/original-3750691-1.jpg"
+                        className="feature-image"
+                        alt="feature" />
+                        <h4 className="feature-title">Summer Lessons Bundles</h4>
+                        <p className="author">Ms. Lopez</p>
+                        <p className="star"><img src={RatingLogo} alt="rating"/>4.8</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
