6355280303a3208a7852f48b6ed68ae02deb2891
james-fleming394 <james.fleming394@gmail.com>
2023-04-19 14:50:28 -0400

    Heading for Lessons page

--- src/pages/Lesson.jsx
+++ src/pages/Lesson.jsx
@@ -1,11 +1,82 @@
 import React from "react";
 import './Lesson.css';
+import RatingLogo from '../assets/star.png';
+import { useNavigate } from "react-router";
 
 const Lesson = () => {
 
+    let navigate = useNavigate();
+
+    const mathClick = () => {
+        navigate('/lessons/math');
+    }
+
+    const englishClick = () => {
+        navigate('/lessons/english');
+    }
+
+    const writingClick = () => {
+        navigate('/lessons/writing');
+    }
+
+    const scienceClick = () => {
+        navigate('/lessons/science');
+    }
+
+    const ssClick = () => {
+        navigate('/lessons/social-studies');
+    }
+
+    const otherClick = () => {
+        navigate('/lessons/other');
+    }
+
     return (
         <div className="lesson-home">
-
+            <div className="lesson-options">
+                <div className="lessons-section" onClick={mathClick}>
+                    <h1 className="h1-lesson">Math Lessons</h1>
+                    <img 
+                    className="lesson-img" 
+                    src="https://www.creativefabrica.com/wp-content/uploads/2022/09/28/Cartoon-maths-elements-background-educa-Graphics-39463579-1.png"
+                    alt="lessons" />
+                </div>
+                <div className="lessons-section" onClick={englishClick}>
+                    <h1 className="h1-lesson">English Lessons</h1>
+                    <img 
+                    className="lesson-img" 
+                    src="https://scontent-lga3-2.xx.fbcdn.net/v/t1.6435-9/35436022_995091660649239_5010228248710217728_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=VHUwcHF6IecAX99LuHh&_nc_ht=scontent-lga3-2.xx&oh=00_AfA8WPMsDEQTin9rXXHKEQNS3mAIlZGrD0g2AjgI4K8MwQ&oe=6467A4B3"
+                    alt="lessons" />
+                </div>
+                <div className="lessons-section" onClick={writingClick}>
+                    <h1 className="h1-lesson">Writing Lessons</h1>
+                    <img 
+                    className="lesson-img" 
+                    src="https://media.istockphoto.com/id/1227423935/vector/happy-cute-cartoon-school-children.jpg?s=612x612&w=0&k=20&c=Ksg_tbxlIkvOZpsGrVzWd78WoX1vETcgpDDtZjr5eog="
+                    alt="lessons" />
+                </div>
+                <div className="lessons-section" onClick={scienceClick}>
+                    <h1 className="h1-lesson">Science Lessons</h1>
+                    <img 
+                    className="lesson-img" 
+                    src="https://img.freepik.com/premium-vector/science-book-about-space-vector-illustration-cartoon-education-concept-with-rocket-planet-star-hand-drawn-satellite-creative-design-about-school-chemical-biology-elements_109722-3341.jpg?w=2000"
+                    alt="lessons" />
+                </div>
+                <div className="lessons-section" onClick={ssClick}>
+                    <h1 className="h1-lesson">Social Studies Lessons</h1>
+                    <img 
+                    className="lesson-img" 
+                    src="https://media.istockphoto.com/id/1178800730/vector/children-having-fun-with-earth.jpg?s=612x612&w=0&k=20&c=jx-BSJg312kHJ3J-BSzQXM2oW7ZAKjU4Xr0aYNgFDX8="
+                    alt="lessons" />
+                </div>
+                <div className="lessons-section" onClick={otherClick}>
+                    <h1 className="h1-lesson">Other Lessons</h1>
+                    <img 
+                    className="lesson-img" 
+                    src="https://static.vecteezy.com/system/resources/previews/014/005/417/original/boys-are-writing-kids-doing-homework-maths-at-home-cartoon-cute-little-boy-in-red-shirt-siting-on-the-desk-the-concept-of-learning-age-illustrations-isolated-on-white-background-vector.jpg"
+                    alt="lessons" />
+                </div>
+            </div>
         </div>
     )
 }
