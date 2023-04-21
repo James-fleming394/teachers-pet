e02d69ba9e1575ad5d529ebf88ab625cd52a9711
james-fleming394 <james.fleming394@gmail.com>
2023-04-21 14:34:08 -0400

    Math lessons page complete

--- src/pages/Lesson-math.jsx
+++ src/pages/Lesson-math.jsx
@@ -27,100 +27,100 @@ const MathLesson = () => {
                 <h1 className="assess-all">Math Lessons</h1>
                 <div className="features-container">
                     <div className="feature">
-                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/Grade-2-Math-Tests-Package-Based-on-the-Ontario-Curriculum-1670415055/original-689458-1.jpg"
+                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/2nd-Grade-Magic-of-Math-Lesson-Plans-for-Shapes-Geometry-and-Fractions-2795672-1657530780/original-2795672-1.jpg"
                         className="feature-image"
                         alt="feature" />
-                        <h4 className="feature-title">Second Grade Math Assessment Bundle</h4>
-                        <p className="author">Ms. Tarnopal</p>
-                        <p className="star"><img className="star-" src={RatingLogo} alt="rating"/>4.8</p>
+                        <h4 className="feature-title">Magic of Math: Shapes & Fractions Lesson</h4>
+                        <p className="author">Ms. Munson</p>
+                        <p className="star"><img className="star-" src={RatingLogo} alt="rating"/>4.9</p>
                     </div>
                     <div className="feature">
-                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/Test-Prep-Motivational-Testing-Notes-FREEBIE-2337294-1657122536/original-2337294-1.jpg"
+                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/2nd-Grade-Magic-of-Math-Lesson-Plans-for-Place-Value-2550454-1657270117/original-2550454-1.jpg"
                         className="feature-image"
                         alt="feature" />
-                        <h4 className="feature-title">Teacher Test Prep Motivations</h4>
-                        <p className="author">Mr. Fleming</p>
-                        <p className="star"><img src={RatingLogo} alt="rating"/>4.7</p>
-                    </div>
-                    <div className="feature">
-                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/Test-Time-Sunshine-for-Teachers-Treat-Tags-for-Your-Colleagues-2495820-1657123923/original-2495820-1.jpg"
-                        className="feature-image"
-                        alt="feature" />
-                        <h4 className="feature-title">Test-Time Sunshine for Teachers!</h4>
-                        <p className="author">Ms. Cavataio</p>
-                        <p className="star"><img src={RatingLogo} alt="rating"/>4.9</p>
-                    </div>
-                    <div className="feature">
-                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/MATH-TEST-PREP-5TH-GRADE-2217349-1657121845/original-2217349-1.jpg"
-                        className="feature-image"
-                        alt="feature" />
-                        <h4 className="feature-title">Fifth Grade Math Crunch Time Test Prep</h4>
-                        <p className="author">Ms. Spina</p>
-                        <p className="star"><img src={RatingLogo} alt="rating"/>5.0</p>
-                    </div>
-                    <div className="feature">
-                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/New-York-State-Math-Practice-Tests-for-3rd-Grade-No-Prep--2455415-1661676477/original-2455415-1.jpg"
-                        className="feature-image"
-                        alt="feature" />
-                        <h4 className="feature-title">Np Prep Test Prep: Grade 3 Math</h4>
-                        <p className="author">Mr. Chaitin</p>
-                        <p className="star"><img src={RatingLogo} alt="rating"/>4.7</p>
-                    </div>
-                    <div className="feature">
-                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/6th-Grade-Math-Test-Prep-End-of-Year-Review-4383768-1679912361/original-4383768-1.jpg"
-                        className="feature-image"
-                        alt="feature" />
-                        <h4 className="feature-title">6th Grade Math Test Prep Review</h4>
+                        <h4 className="feature-title">Magic of Math: Place Value Lessons</h4>
                         <p className="author">Ms. Munson</p>
                         <p className="star"><img src={RatingLogo} alt="rating"/>4.8</p>
                     </div>
                     <div className="feature">
-                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/Standardized-Test-Key-Word-Posters-1657111731/original-531458-1.jpg"
+                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/1st-and-2nd-Grade-Math-Games-Centers-Stations-Review-Toothy-Bundle-3037141-1679907233/original-3037141-1.jpg"
                         className="feature-image"
                         alt="feature" />
-                        <h4 className="feature-title">Test Terms: <br></br>Posters & Reference Pages</h4>
-                        <p className="author">Mr. Bautista</p>
-                        <p className="star"><img src={RatingLogo} alt="rating"/>4.5</p>
+                        <h4 className="feature-title">Math Lessons & Games</h4>
+                        <p className="author">Ms. Cavataio</p>
+                        <p className="star"><img src={RatingLogo} alt="rating"/>4.9</p>
                     </div>
                     <div className="feature">
-                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/Standardized-Test-Prep-UNIT-Testing-as-A-Genre-Daily-Mini-Lessons-BUNDLE-5257202-1657589445/original-5257202-1.jpg"
+                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/FirstieMath-Intervention-Curriculum-3220934-1676391898/original-3220934-1.jpg"
                         className="feature-image"
                         alt="feature" />
-                        <h4 className="feature-title">Standardized Test Prep Unit: <br></br>Stratgies, Lesson Plans and Activities</h4>
-                        <p className="author">Ms. Hall</p>
+                        <h4 className="feature-title">Math Intervention Lessons</h4>
+                        <p className="author">Ms. Spina</p>
                         <p className="star"><img src={RatingLogo} alt="rating"/>4.4</p>
                     </div>
                     <div className="feature">
-                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/Motivational-Testing-Notes-for-Student-Encouragement-State-Testing-Motivation-696615-1678795406/original-696615-1.jpg"
+                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/3rd-Grade-Magic-of-Math-Lesson-Plans-for-Geometry-Area-and-Perimeter-2989347-1657531563/original-2989347-1.jpg"
                         className="feature-image"
                         alt="feature" />
-                        <h4 className="feature-title">Testing Desk Treats: <br></br>35 Encouraging Notes</h4>
+                        <h4 className="feature-title">Magic of Math: Area & Perimeter</h4>
+                        <p className="author">Mr. Chaitin</p>
+                        <p className="star"><img src={RatingLogo} alt="rating"/>4.8</p>
+                    </div>
+                    <div className="feature">
+                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/2nd-Grade-Magic-of-Math-Lesson-Plans-for-Addition-and-Subtraction-Regrouping-2851279-1675258869/original-2851279-1.jpg"
+                        className="feature-image"
+                        alt="feature" />
+                        <h4 className="feature-title">Magic of Math: Regrouping</h4>
+                        <p className="author">Ms. Munson</p>
+                        <p className="star"><img src={RatingLogo} alt="rating"/>4.9</p>
+                    </div>
+                    <div className="feature">
+                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/Digital-Math-Warm-Ups-First-Grade-3660036-1667312280/original-3660036-1.jpg"
+                        className="feature-image"
+                        alt="feature" />
+                        <h4 className="feature-title">Math Warm-Up Lessons</h4>
+                        <p className="author">Mr. Bautista</p>
+                        <p className="star"><img src={RatingLogo} alt="rating"/>4.6</p>
+                    </div>
+                    <div className="feature">
+                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/2nd-Grade-Magic-of-Math-Lesson-Plans-for-the-ENTIRE-YEAR-3118532-1680686367/original-3118532-1.jpg"
+                        className="feature-image"
+                        alt="feature" />
+                        <h4 className="feature-title">Magic of Math: Year Bundle</h4>
+                        <p className="author">Ms. Munson</p>
+                        <p className="star"><img src={RatingLogo} alt="rating"/>5.0</p>
+                    </div>
+                    <div className="feature">
+                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/Math-Curriculum-Growing-Bundle-THIRD-GRADE-7100872-1673395705/original-7100872-1.jpg"
+                        className="feature-image"
+                        alt="feature" />
+                        <h4 className="feature-title">Math Curriculum: Full Year Bundle</h4>
                         <p className="author">Ms. Russo</p>
                         <p className="star"><img src={RatingLogo} alt="rating"/>4.9</p>
                     </div>
                     <div className="feature">
-                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/Do-Not-Disturb-Testing-Signs-5046654-1657142584/original-5046654-1.jpg"
+                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/2nd-Grade-Math-Math-Made-Fun-Curriculum-3859970-1672536906/original-3859970-1.jpg"
                         className="feature-image"
                         alt="feature" />
-                        <h4 className="feature-title">Do Not Disturb Testing Signs</h4>
+                        <h4 className="feature-title">Second Grade Math Curriculum Bundle</h4>
                         <p className="author">Ms. Katz</p>
                         <p className="star"><img src={RatingLogo} alt="rating"/>4.8</p>
                     </div>
                     <div className="feature">
-                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/Nonfiction-Text-Structure-Game-Show-Test-Prep-Reading-Review-Game-Digital-3003374-1678869086/original-3003374-1.jpg"
+                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/-2nd-Grade-Magic-of-Math-Lesson-Plans-for-Data-and-Graphs-3043341-1678703502/original-3043341-1.jpg"
                         className="feature-image"
                         alt="feature" />
-                        <h4 className="feature-title">Text Structure: Game Show</h4>
-                        <p className="author">Ms. Garcia</p>
-                        <p className="star"><img src={RatingLogo} alt="rating"/>4.3</p>
+                        <h4 className="feature-title">Magic of Math: Graphing</h4>
+                        <p className="author">Ms. Brennan</p>
+                        <p className="star"><img src={RatingLogo} alt="rating"/>4.6</p>
                     </div>
                     <div className="feature">
-                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/Reading-Test-Prep-Task-Cards-Printable-Google-Forms-and-Slides-3671113-1673027655/original-3671113-1.jpg"
+                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/3rd-Grade-Magic-of-Math-Lesson-Plans-for-Multiplication-2808290-1657530860/original-2808290-1.jpg"
                         className="feature-image"
                         alt="feature" />
-                        <h4 className="feature-title">Test Prep: Reading Task Cards</h4>
-                        <p className="author">Ms. Wiley</p>
-                        <p className="star"><img src={RatingLogo} alt="rating"/>3.9</p>
+                        <h4 className="feature-title">Magic of Math: Multiplication</h4>
+                        <p className="author">Ms. Hall</p>
+                        <p className="star"><img src={RatingLogo} alt="rating"/>4.5</p>
                     </div>
                 </div>
             </div>
