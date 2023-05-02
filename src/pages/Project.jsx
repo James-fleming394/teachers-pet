103a7dedcbb6e7b749deb12f654a55d785978da5
james-fleming394 <james.fleming394@gmail.com>
2023-05-02 18:03:07 -0400

    Social studies project quick view complete

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -197,27 +197,27 @@ const Projects = () => {
                 <h1 className="assess-all">Featured Social Studies Projects</h1>
                 <div className="features-container">
                     <div className="feature">
-                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/Little-SOCIAL-STUDIES-Thinkers-UNIT-1-Citizenship-Kindergarten-Social-Studies--2689773-1664967206/original-2689773-1.jpg"
+                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/Ancient-India-Interactive-Notebook-2173287-1645377586/original-2173287-1.jpg"
                         className="feature-image"
                         alt="feature" />
-                        <h4 className="feature-title">Social Studies: Citizenship Lessons</h4>
-                        <p className="author">Ms. Scott</p>
-                        <p className="star"><img className="star-" src={RatingLogo} alt="rating"/>4.6</p>
+                        <h4 className="feature-title">Interactive Project: Ancient India</h4>
+                        <p className="author">Ms. Cornell</p>
+                        <p className="star"><img className="star-" src={RatingLogo} alt="rating"/>4.9</p>
                     </div>
                     <div className="feature">
-                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/Colony-Unit-filled-with-Informational-Text-Quiz-Test-Project-1668958073/original-510660-1.jpg"
+                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/Land-Acknowledgement-Poster-Announcement-Project-Grade-5-Social-Studies--5831198-1658779900/original-5831198-1.jpg"
                         className="feature-image"
                         alt="feature" />
-                        <h4 className="feature-title">Social Studies: Colonial Unit Lessons</h4>
-                        <p className="author">Ms. Weinberg</p>
-                        <p className="star"><img src={RatingLogo} alt="rating"/>4.9</p>
+                        <h4 className="feature-title">Land Acknowledgement Action Project</h4>
+                        <p className="author">Ms. Sainz</p>
+                        <p className="star"><img src={RatingLogo} alt="rating"/>4.8</p>
                     </div>
                     <div className="feature">
-                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/Holidays-and-Christmas-Around-the-World-Digital-and-Printable-2885336-1671833250/original-2885336-1.jpg"
+                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/Country-Research-Project-A-Social-Studies-Report-4679661-1652175531/original-4679661-1.jpg"
                         className="feature-image"
                         alt="feature" />
-                        <h4 className="feature-title">Holidays Around the World Lesson</h4>
-                        <p className="author">Ms. Cayne</p>
+                        <h4 className="feature-title">My Country: Research Project</h4>
+                        <p className="author">Ms. Wiley</p>
                         <p className="star"><img src={RatingLogo} alt="rating"/>4.7</p>
                     </div>
                 </div>
