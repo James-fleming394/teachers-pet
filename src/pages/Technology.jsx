e6943d3ff3028bdc770aff2bff8dccd17eb8d964
james-fleming394 <james.fleming394@gmail.com>
2023-05-05 12:19:51 -0400

    Navigate links for tech set up

--- src/pages/Technology.jsx
+++ src/pages/Technology.jsx
@@ -1,7 +1,35 @@
 import React from "react";
 import './Lesson.css';
+import { useNavigate } from "react-router";
 
-const Technology = () => {
+const Technology = () => { 
+
+    let navigate = useNavigate();
+
+    const mathClick = () => {
+        navigate('/technology/math');
+    }
+
+    const englishClick = () => {
+        navigate('/technology/english');
+    }
+
+    const writingClick = () => {
+        navigate('/technology/writing');
+    }
+
+    const scienceClick = () => {
+        navigate('/technology/science');
+    }
+
+    const ssClick = () => {
+        navigate('/technology/social-studies');
+    }
+
+    const otherClick = () => {
+        navigate('/technology/other');
+    }
+
 
     return(
         <div>
