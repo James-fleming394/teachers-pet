8d38e2b4a1a50187b6c91d83c0fb4b076a92f12f
james-fleming394 <james.fleming394@gmail.com>
2023-05-05 13:12:28 -0400

    Tech landing page complete

--- src/pages/Technology.jsx
+++ src/pages/Technology.jsx
@@ -82,26 +82,26 @@ const Technology = () => {
                 <h1 className="assess-all">Featured Math Technologies</h1>
                 <div className="features-container">
                     <div className="feature">
-                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/Place-Value-Detective-A-Place-Value-Project-Enrichment-Centers-Small-Group-056730700-1375389930-1667869978/original-804151-1.jpg"
+                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/Digital-Math-Game-Mystery-Multiplication-CSI-Escape-Room-Math-Differentiated-4221187-1665795220/original-4221187-1.jpg"
                         className="feature-image"
                         alt="feature" />
-                        <h4 className="feature-title">Place Value Detective Project</h4>
-                        <p className="author">Ms. Byrant</p>
+                        <h4 className="feature-title">CSI Math Game: Digital</h4>
+                        <p className="author">Ms. Smith</p>
                         <p className="star"><img className="star-" src={RatingLogo} alt="rating"/>4.9</p>
                     </div>
                     <div className="feature">
-                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/GEOMETROCITY-Create-Build-a-City-Made-of-Math-Using-Geometry-1339693-1645475556/original-1339693-1.jpg"
+                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/Place-Value-Game-3rd-Grade-CSI-Math-Mystery-Activity-2139030-1658705782/original-2139030-1.jpg"
                         className="feature-image"
                         alt="feature" />
-                        <h4 className="feature-title">Geometrocity Project</h4>
-                        <p className="author">Mr. Hanley</p>
-                        <p className="star"><img src={RatingLogo} alt="rating"/>4.7</p>
+                        <h4 className="feature-title">Math Mystery: Digital Review</h4>
+                        <p className="author">Mr. Omega</p>
+                        <p className="star"><img src={RatingLogo} alt="rating"/>4.8</p>
                     </div>
                     <div className="feature">
-                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/Multiplication-and-Division-Project-1949479-1682172914/original-1949479-1.jpg"
+                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/Multiplication-Math-Mystery-Game-Multiplying-with-Larger-Numbers-4th-Grade--2354979-1658684401/original-2354979-1.jpg"
                         className="feature-image"
                         alt="feature" />
-                        <h4 className="feature-title">Video Game Truck: Math Project</h4>
+                        <h4 className="feature-title">Monster Mix Up: Digital</h4>
                         <p className="author">Ms. Fagan</p>
                         <p className="star"><img src={RatingLogo} alt="rating"/>4.9</p>
                     </div>
@@ -111,26 +111,26 @@ const Technology = () => {
                 <h1 className="assess-all">Featured English Technologies</h1>
                 <div className="features-container">
                     <div className="feature">
-                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/Afro-Latinos-Printable-Research-Poster-Project-SPANISH-AND-ENGLISH-4340086-1678206930/original-4340086-1.jpg"
+                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/Cause-and-Effect-GAME-SHOW-2nd-4th-Grade-w-Contestants-interactive-ppt-1240213-1589837401/original-1240213-1.jpg"
                         className="feature-image"
                         alt="feature" />
-                        <h4 className="feature-title">Black History Month Research Project</h4>
-                        <p className="author">Ms. Smith</p>
+                        <h4 className="feature-title">Cause and Effect Game Show</h4>
+                        <p className="author">Ms. Strong</p>
                         <p className="star"><img className="star-" src={RatingLogo} alt="rating"/>4.8</p>
                     </div>
                     <div className="feature">
-                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/Animal-Research-Project-Spanish-English-BUNDLE-1852863-1665251409/original-1852863-1.jpg"
+                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/Letter-Fluency-Fitness-Bundle-1657218847/original-1386463-1.jpg"
                         className="feature-image"
                         alt="feature" />
-                        <h4 className="feature-title">My Animal Research Project</h4>
+                        <h4 className="feature-title">Fluency & Fitness Brain Breaks</h4>
                         <p className="author">Ms. Kelly</p>
                         <p className="star"><img src={RatingLogo} alt="rating"/>4.9</p>
                     </div>
                     <div className="feature">
-                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/English-Language-Arts-Choice-Board-Project-Based-Summer-or-Distance-Learning-5596185-1597422873/original-5596185-1.jpg"
+                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/Low-Prep-Classroom-Escape-Room-End-of-Year-Reading-Review-3759516-1657145829/original-3759516-1.jpg"
                         className="feature-image"
                         alt="feature" />
-                        <h4 className="feature-title">Langauge Arts Choice Board</h4>
+                        <h4 className="feature-title">Reading Review: Digital</h4>
                         <p className="author">Ms. Tarnopal</p>
                         <p className="star"><img src={RatingLogo} alt="rating"/>4.8</p>
                     </div>
@@ -143,15 +143,15 @@ const Technology = () => {
                         <img src="https://ecdn.teacherspayteachers.com/thumbitem/Easter-Craft-Activities-Writing-Prompt-Art-Project-Easter-Bunny-House-for-Sale-158880-1681213614/original-158880-1.jpg"
                         className="feature-image"
                         alt="feature" />
-                        <h4 className="feature-title">End of Year Classroom Project</h4>
+                        <h4 className="feature-title">End of Year Classroom Activity</h4>
                         <p className="author">Ms. Sandler</p>
                         <p className="star"><img className="star-" src={RatingLogo} alt="rating"/>4.8</p>
                     </div>
                     <div className="feature">
-                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/-Animal-Reports-A-Lapbook-Animal-Research-Project-Informational-Writing-1771546-1676851428/original-1771546-1.jpg"
+                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/Action-Verb-Unit-1652097689/original-131310-1.jpg"
                         className="feature-image"
                         alt="feature" />
-                        <h4 className="feature-title">Animals Guided Research & Writing Project</h4>
+                        <h4 className="feature-title">Action Verbs Digital Activity</h4>
                         <p className="author">Ms. O'Hare</p>
                         <p className="star"><img src={RatingLogo} alt="rating"/>4.9</p>
                     </div>
@@ -159,7 +159,7 @@ const Technology = () => {
                         <img src="https://ecdn.teacherspayteachers.com/thumbitem/EARTH-DAY-ACTIVITIES-craft-writing-prompt-lesson-flip-book-coloring-art-project-105282-1682428128/original-105282-1.jpg"
                         className="feature-image"
                         alt="feature" />
-                        <h4 className="feature-title">How To Build a Taco: Writing Project</h4>
+                        <h4 className="feature-title">How To Build a Taco: Digital Activity</h4>
                         <p className="author">Ms. Gould</p>
                         <p className="star"><img src={RatingLogo} alt="rating"/>4.8</p>
                     </div>
@@ -172,7 +172,7 @@ const Technology = () => {
                         <img src="https://ecdn.teacherspayteachers.com/thumbitem/Animal-Adaptations-l-Science-Writing-and-Art-Create-a-Creature-Project--1826571-1669198904/original-1826571-1.jpg"
                         className="feature-image"
                         alt="feature" />
-                        <h4 className="feature-title">Create a Creature: Science Project</h4>
+                        <h4 className="feature-title">Create a Creature: Science Digital</h4>
                         <p className="author">Mr. Chaitin</p>
                         <p className="star"><img className="star-" src={RatingLogo} alt="rating"/>4.7</p>
                     </div>
@@ -180,7 +180,7 @@ const Technology = () => {
                         <img src="https://ecdn.teacherspayteachers.com/thumbitem/Human-Body-Organ-System-Poster-Project-1615196-1664360986/original-1615196-1.jpg"
                         className="feature-image"
                         alt="feature" />
-                        <h4 className="feature-title">Human Body: Organ System Project</h4>
+                        <h4 className="feature-title">Human Body: Organ System Digital Activity</h4>
                         <p className="author">Ms. Chdyl</p>
                         <p className="star"><img src={RatingLogo} alt="rating"/>4.9</p>
                     </div>
@@ -188,7 +188,7 @@ const Technology = () => {
                         <img src="https://ecdn.teacherspayteachers.com/thumbitem/Solar-System-Craft-ivity-1658604854/original-589657-1.jpg"
                         className="feature-image"
                         alt="feature" />
-                        <h4 className="feature-title">Solar System & Planets Project</h4>
+                        <h4 className="feature-title">Solar System & Planets Digital Project</h4>
                         <p className="author">Ms. Brown</p>
                         <p className="star"><img src={RatingLogo} alt="rating"/>4.8</p>
                     </div>
@@ -201,7 +201,7 @@ const Technology = () => {
                         <img src="https://ecdn.teacherspayteachers.com/thumbitem/Ancient-India-Interactive-Notebook-2173287-1645377586/original-2173287-1.jpg"
                         className="feature-image"
                         alt="feature" />
-                        <h4 className="feature-title">Interactive Project: Ancient India</h4>
+                        <h4 className="feature-title">Interactive Online Project: Ancient India</h4>
                         <p className="author">Ms. Cornell</p>
                         <p className="star"><img className="star-" src={RatingLogo} alt="rating"/>4.9</p>
                     </div>
@@ -209,7 +209,7 @@ const Technology = () => {
                         <img src="https://ecdn.teacherspayteachers.com/thumbitem/Land-Acknowledgement-Poster-Announcement-Project-Grade-5-Social-Studies--5831198-1658779900/original-5831198-1.jpg"
                         className="feature-image"
                         alt="feature" />
-                        <h4 className="feature-title">Land Acknowledgement Action Project</h4>
+                        <h4 className="feature-title">Land Acknowledgement Digital Action Project</h4>
                         <p className="author">Ms. Sainz</p>
                         <p className="star"><img src={RatingLogo} alt="rating"/>4.8</p>
                     </div>
@@ -227,10 +227,10 @@ const Technology = () => {
                 <h1 className="assess-all">Featured Other Technologies</h1>
                 <div className="features-container">
                     <div className="feature">
-                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/Summer-Bucket-List-Craft-End-of-the-Year-Activities-May-Bulletin-Board-Project-1222474-1682884589/original-1222474-1.jpg"
+                        <img src="https://ecdn.teacherspayteachers.com/thumbitem/Would-You-Rather-Questions-204-Cards-Print-Digital-Google-Slides-or-Easel-52805-1678808432/original-52805-1.jpg"
                         className="feature-image"
                         alt="feature" />
-                        <h4 className="feature-title">Summer Bucket List Project</h4>
+                        <h4 className="feature-title">Would you Rather Tech Game</h4>
                         <p className="author">Ms. O'Neill</p>
                         <p className="star"><img className="star-" src={RatingLogo} alt="rating"/>4.9</p>
                     </div>
@@ -238,7 +238,7 @@ const Technology = () => {
                         <img src="https://ecdn.teacherspayteachers.com/thumbitem/Create-a-Board-Game-Project-Based-Learning-Unit-Templates-Rubrics-EDITABLE-3108396-1670631850/original-3108396-1.jpg"
                         className="feature-image"
                         alt="feature" />
-                        <h4 className="feature-title">Create a Boardgame: Math Project</h4>
+                        <h4 className="feature-title">Create a Boardgame: Digital Math Project</h4>
                         <p className="author">Ms. Norris</p>
                         <p className="star"><img src={RatingLogo} alt="rating"/>4.9</p>
                     </div>
@@ -246,7 +246,7 @@ const Technology = () => {
                         <img src="https://ecdn.teacherspayteachers.com/thumbitem/The-Game-of-Life-Financial-Literacy-Project-1639002404/original-627444-1.jpg"
                         className="feature-image"
                         alt="feature" />
-                        <h4 className="feature-title">Game of Life: Financial Project</h4>
+                        <h4 className="feature-title">Game of Life: Digital Financial Project</h4>
                         <p className="author">Ms. DeSimone</p>
                         <p className="star"><img src={RatingLogo} alt="rating"/>4.8</p>
                     </div>
