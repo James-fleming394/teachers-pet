a9fb9075335eab73b7ff59f4845074b54fbd805c
james-fleming394 <james.fleming394@gmail.com>
2023-05-15 10:20:42 -0400

    Rewards download button

--- src/pages/Reward.jsx
+++ src/pages/Reward.jsx
@@ -41,6 +41,7 @@ const Rewards = () => {
                         <h4 className="feature-title">Reward Coupons Multi-Age</h4>
                         <p className="author">Mr. Chaitin</p>
                         <p className="star"><img src={RatingLogo} alt="rating"/>4.8</p>
+                        <DownloadButton />
                     </div>
                     <div className="feature">
                         <img src="https://ecdn.teacherspayteachers.com/thumbitem/Whole-Class-Rewards-System-4626267-1670538660/original-4626267-1.jpg"
@@ -49,6 +50,7 @@ const Rewards = () => {
                         <h4 className="feature-title">Whole Class Rewards System</h4>
                         <p className="author">Ms. Cavataio</p>
                         <p className="star"><img src={RatingLogo} alt="rating"/>4.9</p>
+                        <DownloadButton />
                     </div>
                     <div className="feature">
                         <img src="https://ecdn.teacherspayteachers.com/thumbitem/Class-Coupons-1989309-1681464471/original-1989309-1.jpg"
@@ -57,6 +59,7 @@ const Rewards = () => {
                         <h4 className="feature-title">Reward Coupons: Editable Choices</h4>
                         <p className="author">Ms. Jacaruso</p>
                         <p className="star"><img src={RatingLogo} alt="rating"/>5.0</p>
+                        <DownloadButton />
                     </div>
                     <div className="feature">
                         <img src="https://ecdn.teacherspayteachers.com/thumbitem/Classroom-Economy-System-Reward-Coupons-Class-Cash-EDITABLE-Coupons-4671604-1683559887/original-4671604-1.jpg"
@@ -65,6 +68,7 @@ const Rewards = () => {
                         <h4 className="feature-title">Classroom Economy Cash System</h4>
                         <p className="author">Mr. Clark</p>
                         <p className="star"><img src={RatingLogo} alt="rating"/>4.8</p>
+                        <DownloadButton />
                     </div>
                     <div className="feature">
                         <img src="https://ecdn.teacherspayteachers.com/thumbitem/Whole-Class-Reward-System-Build-a-Reward-Classroom-Behavior-Management-Bundle-7513520-1683636602/original-7513520-1.jpg"
@@ -73,6 +77,7 @@ const Rewards = () => {
                         <h4 className="feature-title">Build A Reward System</h4>
                         <p className="author">Ms. Abinakad</p>
                         <p className="star"><img src={RatingLogo} alt="rating"/>4.8</p>
+                        <DownloadButton />
                     </div>
                     <div className="feature">
                         <img src="https://ecdn.teacherspayteachers.com/thumbitem/Visual-Reward-Charts-2556180-1657567749/original-2556180-1.jpg"
@@ -81,6 +86,7 @@ const Rewards = () => {
                         <h4 className="feature-title">Token Board: Whole Class Reward System</h4>
                         <p className="author">Ms. Munson</p>
                         <p className="star"><img src={RatingLogo} alt="rating"/>4.7</p>
+                        <DownloadButton />
                     </div>
                     <div className="feature">
                         <img src="https://ecdn.teacherspayteachers.com/thumbitem/Sticker-Charts-Blank-and-Goal-Sticker-Charts-Editable--3716950-1683122355/original-3716950-1.jpg"
@@ -89,6 +95,7 @@ const Rewards = () => {
                         <h4 className="feature-title">Behavior Goal Reward Charts</h4>
                         <p className="author">Ms. Christen</p>
                         <p className="star"><img src={RatingLogo} alt="rating"/>4.6</p>
+                        <DownloadButton />
                     </div>
                     <div className="feature">
                         <img src="https://ecdn.teacherspayteachers.com/thumbitem/Classroom-Reward-Coupons-Positive-Behavior-Management-Classroom-Incentives-257110-1675248933/original-257110-1.jpg"
@@ -97,6 +104,7 @@ const Rewards = () => {
                         <h4 className="feature-title">Back to School: Class Coupons</h4>
                         <p className="author">Ms. Russo</p>
                         <p className="star"><img src={RatingLogo} alt="rating"/>4.8</p>
+                        <DownloadButton />
                     </div>
                     <div className="feature">
                         <img src="https://ecdn.teacherspayteachers.com/thumbitem/Secret-Reward-Puzzle-Classroom-Management-Tool-Classroom-Reward-Tool-3522745-1673096314/original-3522745-1.jpg"
@@ -105,6 +113,7 @@ const Rewards = () => {
                         <h4 className="feature-title">Secret Reward Puzzle</h4>
                         <p className="author">Ms. Katz</p>
                         <p className="star"><img src={RatingLogo} alt="rating"/>4.8</p>
+                        <DownloadButton />
                     </div>
                     <div className="feature">
                         <img src="https://ecdn.teacherspayteachers.com/thumbitem/Incentive-Behavior-Reward-Sticker-Charts-2103185-1675729918/original-2103185-1.jpg"
@@ -113,6 +122,7 @@ const Rewards = () => {
                         <h4 className="feature-title">Incentive Sticker Charts</h4>
                         <p className="author">Ms. Rupp</p>
                         <p className="star"><img src={RatingLogo} alt="rating"/>4.6</p>
+                        <DownloadButton />
                     </div>
                     <div className="feature">
                         <img src="https://ecdn.teacherspayteachers.com/thumbitem/Reward-Tag-BUNDLE-2615821-1656583971/original-2615821-1.jpg"
@@ -121,6 +131,7 @@ const Rewards = () => {
                         <h4 className="feature-title">Reward Tags Huge Bundle</h4>
                         <p className="author">Ms. Chase</p>
                         <p className="star"><img src={RatingLogo} alt="rating"/>4.9</p>
+                        <DownloadButton />
                     </div>
                 </div>
             </div>
